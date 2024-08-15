492f042d1d654da1108a571348d3dc9ebfc8476e
Isaac <isaac.makinde3@mail.dcu.ie>
2024-08-15 01:02:42 +0100

    added testing for update event

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from datetime import datetime
 app = Flask(__name__)
 CORS(app)
 load_dotenv()
+ACCEPTEDFIELDS = ['title', 'host', 'date', 'venue', 'description', 'category', 'pricing', 'guests']
+
 
 def db_connection():
     conn = None
@@ -38,10 +40,13 @@ def api():
         return jsonify({'message': 'Welcome to the API'})
     
 
-@app.route('/api/events', methods=['GET', 'POST'])
+@app.route('/api/events', methods=['GET', 'POST', 'DELETE'])
 def getAllEvents():
+
+
     conn = db_connection()
     cursor = conn.cursor()
+
     # POST    
     if request.method == 'POST': 
         new_title = request.json['title']
@@ -56,7 +61,8 @@ def getAllEvents():
         ## add data to a database
 
         if not all([new_title, new_host, new_date, new_venue, new_description, new_category, new_pricing, new_guests]):
-            return jsonify({'message': 'Missing data'}), 400
+            return jsonify({'message': 'Missing data',
+                            "data" : request.json}), 400
         
         try :
             # convert date string to date object
@@ -67,6 +73,7 @@ def getAllEvents():
 
 
         try:
+            print(request.json)
             sql = '''INSERT INTO events (title, host, date, venue, description, category, pricing, guests) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'''
             cursor.execute(sql, (new_title, new_host, new_date, new_venue, new_description, new_category, new_pricing, new_guests))
             conn.commit() 
@@ -106,10 +113,15 @@ def getAllEvents():
             return jsonify(events), 200
         else: 
             "Nothing Found", 404
+    elif request.method == 'DELETE':
+        cursor.execute('DELETE FROM events')
+        conn.commit()
+        return jsonify({'message': 'All events have been deleted'}), 200
     
 
 @app.route('/api/events/<string:id>', methods=['GET', 'PUT', 'DELETE'])
 def singleEvent(id):
+
     conn = db_connection()
     cursor = conn.cursor()
     
@@ -130,32 +142,51 @@ def singleEvent(id):
             return jsonify({'message': 'Event not found'}), 404
     
     elif request.method == 'PUT':
-        sql = '''UPDATE events SET title=%s, host=%s, date=%s, venue=%s, description=%s, category=%s, pricing=%s, guests=%s WHERE id=%s'''
-        title = request.json['title']
-        host = request.json['host']
-        date = request.json['date']
-        venue = request.json['venue']
-        description = request.json['description']
-        category = request.json['category']
-        pricing = float(request.json['pricing'])
-        guests = request.json['guests']
-        updated_event = {
-            'id': id,
-            'title': title,
-            'host': host,
-            'date': date,
-            'venue': venue,
-            'description': description,
-            'category': category,
-            'pricing': pricing,
-            'guests': guests
-        }
-        cursor.execute(sql, (title, host, date, venue, description, category, pricing, guests, id))
-        conn.commit()
-        return jsonify(updated_event), 200
+        sql = '''UPDATE events SET'''
+        fields = request.json.keys()
+        params = tuple(request.json.values())
+
+        try :
+            if len(fields) == 0:
+                return jsonify({'message': 'No data to update'}), 400
+            
+            else :
+                for field in fields:
+                    if field not in ACCEPTEDFIELDS:
+                        return jsonify({'message': f'{field} is not a valid field'}), 400
+            
+            for field in fields:
+                if field == 'date':
+                    try:
+                        date_obj = datetime.strptime(request.json[field], '%Y-%m-%d')
+                    except ValueError:
+                        return jsonify({'message': 'Invalid date format, use YYYY-MM-DD'}), 400
+                    
+                if field == 'pricing':
+                    try:
+                        float(request.json[field])
+                    except ValueError:
+                        return jsonify({'message': 'Pricing must be a number'}), 400
+                elif type(request.json[field]) != str:
+                    return jsonify({'message': f'Invalid data type for {field}'}), 400
+                
+            params += (id,)
+        
+            for field in fields:
+                sql += f" {field} =%s,"
+            sql = sql[:-1] + " WHERE id = %s"    
+
+            cursor.execute(sql, params)
+            conn.commit()
+            sql = '''SELECT * FROM events WHERE id = %s'''
+            cursor.execute(sql, (id,))
+            updated_event = cursor.fetchone()
+            return jsonify(updated_event), 200
+        except pymysql.Error as e:
+            print(e)
+            return jsonify({'message': 'Something went wrong'}), 500
 
           
-        return jsonify({'message': 'Event not found'}), 404
     
     elif request.method == 'DELETE':
         ## delete data from a database
---
+++ tests/test_event.py
@@ -0,0 +1,159 @@
+import requests
+
+ENDPOINT = "http://127.0.0.1:5000/api"
+
+
+## delete all events
+
+def test_can_call_endpoint():
+    response = requests.get(f"{ENDPOINT}/events")
+    assert response.status_code == 200
+
+
+def test_clean_db():
+    response = requests.delete(f"{ENDPOINT}/events")
+    assert response.status_code == 200
+
+
+def test_can_get_empty_list():
+    response = requests.get(f"{ENDPOINT}/events")
+    data = response.json()
+    assert len(data) == 0 and response.status_code == 200
+
+def test_can_create_event():
+    
+    payload = {
+  "category": "Celebration",
+  "date": "2024-12-12",
+  "description": "Test",
+  "guests":"Test guest",
+  "host": "Test host ",
+  "pricing": 10.0,
+  "title": "Test Title001", ##  have to constantly change this
+  "venue": "Test Venue"
+}
+    create_event_response = create_event(payload)
+    assert create_event_response.status_code == 201
+    data = create_event_response.json()
+    print(data)
+
+    event_id = data['id']
+    get_event_response = get_event(event_id)
+
+
+    assert get_event_response.status_code == 200
+    get_event_data = get_event_response.json()
+    assert get_event_data['title'] == payload['title']
+    assert get_event_data['date'] == payload['date']
+
+
+def test_given_valid_data_can_update_event():
+    # create a new event
+    event_id, original_payload = create_update_test_event_valid()
+
+    # update the event with new data
+    update_payload ={
+        "date": "2024-10-12",
+        "description": "Test for updating new events",
+        "pricing": 40.0,
+    }
+    update_event_response = update_event(update_payload, event_id)
+    assert update_event_response.status_code == 200
+
+    # check the updated data
+    updated_data = update_event_response.json()
+    updated_data["pricing"] = float(updated_data["pricing"])
+    assert type(updated_data['pricing']) == type(update_payload['pricing'])
+    assert updated_data['pricing'] == update_payload['pricing']
+    assert updated_data['date'] == update_payload['date']
+    assert updated_data['description'] == update_payload['description']
+
+
+    # check the original data
+    assert updated_data['title'] == original_payload['title']
+    assert updated_data['host'] == original_payload['host']
+    assert updated_data['venue'] == original_payload['venue']
+    assert updated_data['category'] == original_payload['category']
+    assert updated_data['guests'] == original_payload['guests']
+    assert updated_data['id'] == event_id
+
+
+
+
+def test_update_with_invalid_data():
+    """Test updating an event with invalid data should fail."""
+    event_id, _  = create_update_test_event_invalid()
+    invalid_payload = {
+        "date": "invalid-date-format",
+        "pricing": "not-a-number"
+    }
+    update_event_response = update_event(invalid_payload, event_id)
+    assert update_event_response.status_code == 400
+
+    # check the original data
+    
+## helper functions
+
+def create_event(payload):
+    return requests.post(f"{ENDPOINT}/events", json=payload)
+
+def get_event(event_id):
+    return requests.get(f"{ENDPOINT}/events/{event_id}")
+
+def update_event(payload, event_id):
+    return requests.put(f"{ENDPOINT}/events/{event_id}", json=payload)
+
+
+def new_event_payload():
+    return {
+  "category": "Celebration",
+  "date": "2024-12-12",
+  "description": "Test for creating new events",
+  "guests":"Test guest",
+  "host": "Test host ",
+  "pricing": 10.0,
+  "title": "Test Title001",
+  "venue": "Test Venue"
+}
+
+
+def create_update_test_event_valid():
+    """Helper function to create a test event"""
+    payload = {
+        "category": "Travel",
+        "date": "2024-12-25",
+        "description": "Test for updating events",
+        "guests":"Test guest",
+        "host": "Test host ",
+        "pricing": 10.0,
+        "title": "Test Title002",
+        "venue": "Test Venue"
+    }
+    response = create_event(payload)
+    assert response.status_code == 201
+    return response.json()["id"], payload
+
+
+
+def create_update_test_event_invalid():
+    """Helper function to create a test event"""
+    payload = {
+        "category": "Travel",
+        "date": "2024-12-25",
+        "description": "Test for updating events given invalid data",
+        "guests":"Test guest",
+        "host": "Test host ",
+        "pricing": 10.0,
+        "title": "Test Title003",
+        "venue": "Test Venue"
+    }
+    response = create_event(payload)
+    assert response.status_code == 201
+    return response.json()["id"], payload
+
+
+   
+    
+
+def delete_events():
+    return requests.delete(f"{ENDPOINT}/events")
