c42bff6a9d578859756e3585ff3945b32dbab7bb
Isaac <isaac.makinde3@mail.dcu.ie>
2024-10-14 20:23:27 +0100

    deconstructed firebase

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,18 @@ def create_app():
     BUCKET_NAME = os.getenv('FIREBASE_BUCKET')
     
     if firebase_creds and BUCKET_NAME:
-        cred_dict = json.loads(firebase_creds)
+        cred_dict = {
+            "type": "service_account",
+            "project_id": os.getenv('FIREBASE_PROJECT_ID'),
+            "private_key_id": os.getenv('FIREBASE_PRIVATE_KEY_ID'),
+            "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+            "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
+            "client_id": os.getenv('FIREBASE_CLIENT_ID'),
+            "auth_uri": os.getenv('FIREBASE_AUTH_URI'),
+            "token_uri": os.getenv('FIREBASE_TOKEN_URI'),
+            "auth_provider_x509_cert_url": os.getenv('FIREBASE_AUTH_PROVIDER'),
+            "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_CERT')
+        }
         cred = credentials.Certificate(cred_dict)
         firebase_admin.initialize_app(cred, {
             "storageBucket": BUCKET_NAME,
